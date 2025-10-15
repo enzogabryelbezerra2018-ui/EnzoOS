4648dcd48f3c383eb932ba3ee3413f59984e0dcf
enzogabryelbezerra2018-ui <enzogabryelbezerra2018@gmail.com>
2025-10-14 23:01:04 -0300

    Create bootable.cr

---
+++ source/system/bootable_select/bootable.cr
@@ -0,0 +1,15 @@
+#include  source/system/new_var
+printg("bootables")
+<opitions>
+enzoOS = "OS padrão"
+</opitions> 
+printg("tem certeza que quer selecionar o sistema configurado? [S/N]")
+
+sim var = S = 0x000000S
+não var = N = 0x000000N
+
+printg("+ OS") logic_new(
+  new var = 0x000000NEW
+  execute = new_var.dec
+  execute var = 0x000000EXEC
+)
